87a5fbef3d10c652d555d3de83a803f067b04a10
jason <jcwang0919@gmail.com>
2025-08-18 16:35:43 +0800

    Fix Vercel build: Remove problematic webpack ignore patterns causing micromatch RangeError

--- next.config.js
+++ next.config.js
@@ -2,21 +2,6 @@
 const nextConfig = {
   // Remove experimental.appDir as it's now stable in Next.js 14
   
-  // Ignore problematic folders with Chinese characters during build
-  webpack: (config, { isServer }) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: [
-        '**/node_modules/**',
-        '**/.git/**',
-        '**/public/纸盒编辑文件/**',
-        '**/public/刀叉图上传/**',
-        '**/public/Sugarcane Bagasse 上传/**'
-      ]
-    };
-    return config;
-  },
-  
   // Additional configuration to handle static file serving
   async rewrites() {
     return [];
